02fd9c13430ba9e63eeb698086d2d8562e40bb18
Ian Maher <ianpmaher@gmail.com>
2023-12-12 23:22:08 -0500

    need to reconfigure forms

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,44 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface BlogTitle extends Schema.Component {
+  collectionName: 'components_blog_titles';
+  info: {
+    displayName: 'title';
+    icon: 'crop';
+  };
+  attributes: {
+    title: Attribute.String &
+      Attribute.Required &
+      Attribute.DefaultTo<'blog title'>;
+    blogcontent: Attribute.RichText & Attribute.DefaultTo<'blogcontent'>;
+  };
+}
+
+export interface ScholarshipScholarshipType extends Schema.Component {
+  collectionName: 'components_scholarship_scholarship_types';
+  info: {
+    displayName: 'scholarship-type';
+    icon: 'hashtag';
+  };
+  attributes: {
+    title: Attribute.String &
+      Attribute.Required &
+      Attribute.DefaultTo<'scholarshipName'>;
+    description: Attribute.RichText &
+      Attribute.DefaultTo<'scholarship description'>;
+    deadline: Attribute.Date & Attribute.DefaultTo<'2024-12-01'>;
+    color: Attribute.String &
+      Attribute.CustomField<'plugin::color-picker.color'>;
+    value: Attribute.Integer;
+    isActive: Attribute.Boolean & Attribute.DefaultTo<true>;
+  };
+}
+
 declare module '@strapi/types' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'blog.title': BlogTitle;
+      'scholarship.scholarship-type': ScholarshipScholarshipType;
+    }
+  }
 }
--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -362,99 +362,6 @@ export interface AdminTransferTokenPermission extends Schema.CollectionType {
   };
 }
 
-export interface ApiScholarshipScholarship extends Schema.CollectionType {
-  collectionName: 'scholarships';
-  info: {
-    singularName: 'scholarship';
-    pluralName: 'scholarships';
-    displayName: 'scholarships';
-    description: '';
-  };
-  options: {
-    draftAndPublish: true;
-  };
-  attributes: {
-    title: Attribute.String &
-      Attribute.Required &
-      Attribute.SetMinMaxLength<{
-        minLength: 1;
-      }> &
-      Attribute.DefaultTo<'scholarshipName'>;
-    description: Attribute.RichText &
-      Attribute.Required &
-      Attribute.SetMinMaxLength<{
-        minLength: 1;
-      }>;
-    isActive: Attribute.Boolean &
-      Attribute.Required &
-      Attribute.DefaultTo<true>;
-    deadline: Attribute.Date & Attribute.Required;
-    value: Attribute.Integer;
-    pic: Attribute.Media;
-    pdf: Attribute.Media;
-    slug: Attribute.UID<'api::scholarship.scholarship', 'title'>;
-    eligibility: Attribute.Enumeration<
-      ['Merit-Based', 'Needs-Based', 'Merit- & Needs-Based', 'Other']
-    >;
-    createdAt: Attribute.DateTime;
-    updatedAt: Attribute.DateTime;
-    publishedAt: Attribute.DateTime;
-    createdBy: Attribute.Relation<
-      'api::scholarship.scholarship',
-      'oneToOne',
-      'admin::user'
-    > &
-      Attribute.Private;
-    updatedBy: Attribute.Relation<
-      'api::scholarship.scholarship',
-      'oneToOne',
-      'admin::user'
-    > &
-      Attribute.Private;
-    sitemap_exclude: Attribute.Boolean &
-      Attribute.Private &
-      Attribute.DefaultTo<false>;
-  };
-}
-
-export interface ApiScholarshipsLandingScholarshipsLanding
-  extends Schema.SingleType {
-  collectionName: 'scholarships_landings';
-  info: {
-    singularName: 'scholarships-landing';
-    pluralName: 'scholarships-landings';
-    displayName: 'Scholarships Landing';
-  };
-  options: {
-    draftAndPublish: true;
-  };
-  attributes: {
-    scholarships: Attribute.Relation<
-      'api::scholarships-landing.scholarships-landing',
-      'oneToMany',
-      'api::scholarship.scholarship'
-    >;
-    createdAt: Attribute.DateTime;
-    updatedAt: Attribute.DateTime;
-    publishedAt: Attribute.DateTime;
-    createdBy: Attribute.Relation<
-      'api::scholarships-landing.scholarships-landing',
-      'oneToOne',
-      'admin::user'
-    > &
-      Attribute.Private;
-    updatedBy: Attribute.Relation<
-      'api::scholarships-landing.scholarships-landing',
-      'oneToOne',
-      'admin::user'
-    > &
-      Attribute.Private;
-    sitemap_exclude: Attribute.Boolean &
-      Attribute.Private &
-      Attribute.DefaultTo<false>;
-  };
-}
-
 export interface PluginUploadFile extends Schema.CollectionType {
   collectionName: 'files';
   info: {
@@ -861,6 +768,128 @@ export interface PluginSitemapSitemapCache extends Schema.CollectionType {
   };
 }
 
+export interface ApiArticleArticle extends Schema.CollectionType {
+  collectionName: 'articles';
+  info: {
+    singularName: 'article';
+    pluralName: 'articles';
+    displayName: 'scholarshipform';
+    description: '';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    scholarshipsubmit: Attribute.DynamicZone<['scholarship.scholarship-type']> &
+      Attribute.Required;
+    createdAt: Attribute.DateTime;
+    updatedAt: Attribute.DateTime;
+    publishedAt: Attribute.DateTime;
+    createdBy: Attribute.Relation<
+      'api::article.article',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+    updatedBy: Attribute.Relation<
+      'api::article.article',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+    sitemap_exclude: Attribute.Boolean &
+      Attribute.Private &
+      Attribute.DefaultTo<false>;
+  };
+}
+
+export interface ApiScholarshipScholarship extends Schema.CollectionType {
+  collectionName: 'scholarships';
+  info: {
+    singularName: 'scholarship';
+    pluralName: 'scholarships';
+    displayName: 'scholarships';
+    description: '';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    title: Attribute.String &
+      Attribute.Required &
+      Attribute.SetMinMaxLength<{
+        minLength: 1;
+      }> &
+      Attribute.DefaultTo<'scholarshipName'>;
+    description: Attribute.RichText & Attribute.DefaultTo<'transcript'>;
+    isActive: Attribute.Boolean & Attribute.DefaultTo<true>;
+    deadline: Attribute.Date & Attribute.DefaultTo<'2024-12-31'>;
+    value: Attribute.Integer;
+    pic: Attribute.Media;
+    pdf: Attribute.Media;
+    slug: Attribute.UID<'api::scholarship.scholarship', 'title'>;
+    eligibility: Attribute.Enumeration<
+      ['Merit-Based', 'Needs-Based', 'Merit- & Needs-Based', 'Other']
+    >;
+    createdAt: Attribute.DateTime;
+    updatedAt: Attribute.DateTime;
+    publishedAt: Attribute.DateTime;
+    createdBy: Attribute.Relation<
+      'api::scholarship.scholarship',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+    updatedBy: Attribute.Relation<
+      'api::scholarship.scholarship',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+    sitemap_exclude: Attribute.Boolean &
+      Attribute.Private &
+      Attribute.DefaultTo<false>;
+  };
+}
+
+export interface ApiScholarshipsLandingScholarshipsLanding
+  extends Schema.SingleType {
+  collectionName: 'scholarships_landings';
+  info: {
+    singularName: 'scholarships-landing';
+    pluralName: 'scholarships-landings';
+    displayName: 'Scholarships Landing';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    scholarships: Attribute.Relation<
+      'api::scholarships-landing.scholarships-landing',
+      'oneToMany',
+      'api::scholarship.scholarship'
+    >;
+    createdAt: Attribute.DateTime;
+    updatedAt: Attribute.DateTime;
+    publishedAt: Attribute.DateTime;
+    createdBy: Attribute.Relation<
+      'api::scholarships-landing.scholarships-landing',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+    updatedBy: Attribute.Relation<
+      'api::scholarships-landing.scholarships-landing',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+    sitemap_exclude: Attribute.Boolean &
+      Attribute.Private &
+      Attribute.DefaultTo<false>;
+  };
+}
+
 declare module '@strapi/types' {
   export module Shared {
     export interface ContentTypes {
@@ -871,8 +900,6 @@ declare module '@strapi/types' {
       'admin::api-token-permission': AdminApiTokenPermission;
       'admin::transfer-token': AdminTransferToken;
       'admin::transfer-token-permission': AdminTransferTokenPermission;
-      'api::scholarship.scholarship': ApiScholarshipScholarship;
-      'api::scholarships-landing.scholarships-landing': ApiScholarshipsLandingScholarshipsLanding;
       'plugin::upload.file': PluginUploadFile;
       'plugin::upload.folder': PluginUploadFolder;
       'plugin::i18n.locale': PluginI18NLocale;
@@ -881,6 +908,9 @@ declare module '@strapi/types' {
       'plugin::users-permissions.user': PluginUsersPermissionsUser;
       'plugin::sitemap.sitemap': PluginSitemapSitemap;
       'plugin::sitemap.sitemap-cache': PluginSitemapSitemapCache;
+      'api::article.article': ApiArticleArticle;
+      'api::scholarship.scholarship': ApiScholarshipScholarship;
+      'api::scholarships-landing.scholarships-landing': ApiScholarshipsLandingScholarshipsLanding;
     }
   }
 }
